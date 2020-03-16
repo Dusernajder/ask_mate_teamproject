5ce85ec7c38ff6073cffd03f01dc39c952eeccf4
Tomporino <tomorpeter97@gmail.com>
2020-03-16 21:02:51 +0100

    list and submission time

--- data_handler.py
+++ data_handler.py
@@ -2,7 +2,7 @@ import csv
 import os
 
 answers_header = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-question_header = ['id', 'submission_time', 'view_number', 'vote_number', 'title,message', 'image']
+question_header = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
 ANSWER_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
 QUESTION_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
@@ -14,12 +14,12 @@ QUESTION_CSV_LENGTH = sum(1 for row in csv.reader('sample_data/data.csv'))
 path = ANSWER_DATA_FILE_PATH
 
 
-def read_elements_csv(path):
+def read_elements_csv(path, header):
     temp_lst = []
     with open(path) as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
-            dictionary = {key: value for key, value in zip(answers_header, row)}
+            dictionary = {key: value for key, value in zip(header, row)}
             temp_lst.append(dictionary)
         return temp_lst
 
--- server.py
+++ server.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import data_handler
-
+from data_handler import QUESTION_DATA_FILE_PATH, question_header
+import util
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    questions = data_handler.read_elements_csv(data_handler.QUESTION_DATA_FILE_PATH)
+    questions = data_handler.read_elements_csv(QUESTION_DATA_FILE_PATH, question_header)
+    questions = util.convert_timestamp(questions)
     return render_template('home.html', questions=questions)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        debug=True
+    )
