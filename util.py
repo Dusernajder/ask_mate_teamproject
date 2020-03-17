b9179992c4a7939e21a6e501a29b694bac5d5d73
allused <divixed12@gmail.com>
2020-03-17 21:47:27 +0100

    util updated

--- util.py
+++ util.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-
+import time
 
 def convert_timestamp_to_date(data):
     for i in range(len(data)):
         data[i]['submission_time'] = datetime.utcfromtimestamp(int(data[i]['submission_time'])).strftime('%Y-%m-%d')
     return data
+
+def get_unix_time():
+    return str(time.time())[:8]
