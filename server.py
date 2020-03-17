18880d9a87d27689e4d31eacf1027ac1fd6029f8
allused <divixed12@gmail.com>
2020-03-17 21:26:54 +0100

    server.py updated with answer

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    questions = util.convert_timestamp_to_date(data_handler.read_elements_csv(QUESTION_DATA_FILE_PATH))
+    questions = util.convert_timestamp(data_handler.read_elements_csv(QUESTION_DATA_FILE_PATH))
     return render_template('home.html', questions=questions, headers=TEMPLATE_HEADER)
 
 
@@ -43,6 +43,29 @@ def add_question():
 
     return render_template('add_question.html')
 
+@app.route('/answers/<question_id>', methods=['GET', 'POST'])
+def answers(question_id):
+    list_to_csv = []
+    temp_lst = []
+    questions = data_handler.read_elements_csv(QUESTION_DATA_FILE_PATH, )
+    question_dict = data_handler.find_ids(questions, question_id)
+    answers = data_handler.read_elements_csv(ANSWER_DATA_FILE_PATH, )
+
+    if request.method == 'POST':
+        message = request.form['answer_message']
+        image = request.form['img']
+        answer_container = [str(data_handler.get_id(ANSWER_DATA_FILE_PATH)),str(util.get_unix_time()),'0',question_id,message,]
+
+        for item in answer_container:
+            temp_lst.append(item)
+        if image:
+            temp_lst.append(image)
+        print(temp_lst)
+        list_to_csv.append(temp_lst)
+        data_handler.append_csv_by_row(ANSWER_DATA_FILE_PATH, temp_lst)
+        return redirect('/')
+
+    return render_template('answers.html', question=question_dict, answers=answers,)
 
 if __name__ == "__main__":
     app.run(
