19e4b01044441594a34b75c7a6c71242a398bde6
Tomporino <tomorpeter97@gmail.com>
2020-03-20 01:11:48 +0100

    util ts convert

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ from time import time
 import data_handler
 
 
-def convert_timestamp_to_date(data):
+def convert_timestamp_to_date_dict(data):
     for i in range(len(data)):
         data[i]['submission_time'] = datetime.utcfromtimestamp(int(data[i]['submission_time'])).strftime('%Y-%m-%d')
     return data
@@ -32,3 +32,7 @@ def remove_answers(question_id):
         if answer['id'] == question_id:
             del answers[i]
     return answers
+
+
+def convert_timestamp_to_date_string(ts):
+    return datetime.utcfromtimestamp(int(ts)).strftime('%Y-%m-%d')
